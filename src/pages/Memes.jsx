9a5cc308d200d82d6d344b044fa92c8ead665001
Michael <enjoyweaver@gmail.com>
2024-02-25 09:31:07 -0600

    love

--- src/pages/Memes.jsx
+++ src/pages/Memes.jsx
@@ -1,3 +1,6 @@
-const MemesAddresses = ["0xe47d957F83F8887063150AaF7187411351643392"];
+const MemesAddresses = [
+  "0xE388D77715f90Bcf4711EbDB6733A86A7ad79C36",
+  "0xe47d957F83F8887063150AaF7187411351643392",
+];
 
 export default MemesAddresses;
