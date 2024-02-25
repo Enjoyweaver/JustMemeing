5ef6b6574b505ae95d38a4ee8cddc539e45f148b
Michael <enjoyweaver@gmail.com>
2024-02-24 18:24:21 -0600

    vite vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
-})
+  base: "./",
+  build: {
+    rollupOptions: {
+      external: ["web3"], // Add 'web3' to the list of external dependencies
+    },
+  },
+});
