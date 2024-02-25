0317c92372def13716f8b09a4cb34803e97f09c3
Michael <enjoyweaver@gmail.com>
2024-02-24 20:11:15 -0600

    safememes

--- src/pages/Memes.jsx
+++ src/pages/Memes.jsx
@@ -1,6 +1,3 @@
-const MemesAddresses = [
-  "0xb715F8DcE2F0E9b894c753711bd55eE3C04dcA4E",
-  "0xe47d957F83F8887063150AaF7187411351643392",
-];
+const MemesAddresses = ["0xe47d957F83F8887063150AaF7187411351643392"];
 
 export default MemesAddresses;
